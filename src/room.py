0e459a05b6a900942cebdf7e860c2d91be052acb
Elijah-Atkins <elijahorama@gmail.com>
2020-09-08 21:11:49 -0600

    first commit

--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def get_direction(self, direction):
+        if direction == 'n' and hasattr(self, "n_to"):
+            return self.n_to
+        elif direction == 's' and hasattr(self, "s_to"):
+            return self.s_to
+        elif direction == 'e' and hasattr(self, "e_to"):
+            return self.e_to
+        elif direction == 'w' and hasattr(self, "w_to"):
+            return self.w_to
+        else:
+            return None 
